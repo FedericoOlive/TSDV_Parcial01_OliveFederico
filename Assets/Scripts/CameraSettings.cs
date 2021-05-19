2e657d0f9f3db08c3ab0a20711d5aecf02f15742
Federico Olive <FedericoOlive@gmail.com>
2021-05-19 09:37:29 -0300

    Update camera settings.

--- Assets/Scripts/CameraSettings.cs
+++ Assets/Scripts/CameraSettings.cs
@@ -5,25 +5,71 @@ using UnityEngine;
 
 public class CameraSettings : MonoBehaviour
 {
+    enum CameraMode { Perspective, Orthographic }
+
     public GameObject player;
-    [Range(0, 20)] [SerializeField] private float spacingHight;
+    public GameObject floor;
+    [SerializeField] bool playerDependence = true;
+    [Range(0, 40)] [SerializeField] private float spacingHight;
     [Range(0, -10)] [SerializeField] private float spacingFar;
     [Range(-2, 2)] [SerializeField] private float spacingOffset;
+    [SerializeField] private CameraMode cameraMode;
+    [Range(5, 20)] [SerializeField] private int size;
+    private CameraMode lastCameraMode;
+    private Camera cam;
     private Vector3 offset;
-
-    [SerializeField] private float velocity;
-    private PlayerController playerController;
 
     private void Awake()
     {
-        playerController = player.GetComponent<PlayerController>();
+        cam = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        lastCameraMode = cameraMode;
     }
 
     void Update()
     {
-        offset = new Vector3(spacingOffset, spacingHight, spacingFar);
+        SetPositionAndRotation();
 
-        transform.position = player.transform.position + offset;
-        transform.LookAt(player.transform);
+        if (lastCameraMode != cameraMode)
+        {
+            SetCameraMode();
+        }
+    }
+
+    void SetPositionAndRotation()
+    {
+        if (playerDependence)
+        {
+            offset = new Vector3(spacingOffset, spacingHight, spacingFar);
+
+            transform.position = player.transform.position + offset;
+            transform.LookAt(player.transform);
+        }
+        else
+        {
+            offset = new Vector3(spacingOffset, spacingHight, spacingFar);
+
+            transform.position = floor.transform.position + offset;
+            transform.LookAt(floor.transform);
+        }
+        
+        cam.orthographicSize = size;
+    }
+
+    void SetCameraMode()
+    {
+        lastCameraMode = cameraMode;
+        switch (cameraMode)
+        {
+            case CameraMode.Perspective:
+                cam.orthographic = false;
+                break;
+            case CameraMode.Orthographic:
+                cam.orthographic = true;
+                break;
+        }
     }
 }
